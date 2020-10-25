905bbe890194616c83d54545d8f0a1b489d39729
guillaumeteissier <guillaume0teissier@gmail.com>
2020-10-25 17:55:25 +0100

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -988,3 +988,132 @@ if __name__ == '__main__':
     tree = etree.ElementTree(etree.fromstring(xml))
     depth(tree.getroot(), -1)
     print(maxdepth)
+
+    
+    
+# SECTION: Challenges
+
+# Birthday Cake Candles
+import math
+import os
+import random
+import re
+import sys
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    candle_list = sorted(list(candles))
+    return candle_list.count(candle_list[-1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+# Number Line Jumps
+# Problem with the run time 
+
+import math
+import os
+import random
+import re
+import sys
+
+x1, v1, x2, v2 = map(int, input().split(" "))
+if (x1-x2) / (v2-v1) <0:
+    print("NO")
+else:
+    if ((x1-x2) % (v2-v1)) == 0:
+        print("YES")
+    else:
+        print("NO")
+
+        
+#Viral Advertising
+import math
+import os
+import random
+import re
+import sys
+
+def viralAdvertising(n):
+    shared = [5]
+    liked = []
+
+    for x in range(n):
+        liked.append(math.floor(shared[-1]/2))
+        shared.append(liked[-1]*3)
+
+    return(sum(liked))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+# Recursive Digit Sum
+import math
+import os
+import random
+import re
+import sys
+
+def superDigit(n, k):
+    p = n*k
+    while int(p) >= 10:
+        p = str(sum(map(int,list(p))))
+    return p
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    nk = input().split()
+    n = nk[0]
+    k = int(nk[1])
+    result = superDigit(n, k)
+    fptr.write(str(result) + '\n')
+    fptr.close()
+
+# Insertion Sort - Part 1
+# 
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the insertionSort1 function below.
+# Problem with the run time 
+
+def insertionSort1(n, arr):
+    value = arr[n-1]
+    cur_index_value = n-1
+
+    while value < arr[cur_index_value-1]:
+        arr.pop(cur_index_value)
+        arr.insert(cur_index_value-1, arr[cur_index_value-1])
+        cur_index_value = cur_index_value -1
+        print(*arr)
+    print(*arr[0:cur_index_value],value,*arr[cur_index_value+1:])
+        
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
+
+
