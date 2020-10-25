5c24b338494584b01d29266e6836e48538b803fb
guillaumeteissier <guillaume0teissier@gmail.com>
2020-10-25 17:45:54 +0100

    Update Scripts.py

--- Scripts.py
+++ Scripts.py
@@ -964,4 +964,27 @@ if __name__ == '__main__':
     root = tree.getroot()
     print(get_attr_number(root))
 
-# 
+# XML2 - Find the Maximum Depth
+# With the assistance of the solutions for the for loop. 
+
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    if (level == maxdepth):
+        maxdepth += 1
+        
+    for child in elem:
+        depth(child, level + 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
